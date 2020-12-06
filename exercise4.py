eac6ce077673c05e14079ec9e807e756752316c1
izdomi <73383321+izdomi@users.noreply.github.com>
2020-12-06 11:41:16 +0100

    Add files via upload

---
+++ exercise4.py
@@ -0,0 +1,5 @@
+# Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+
+num = int(input("The number: "))
+print([i for i in range(1,num) if num % i == 0])
+# print(lst)
